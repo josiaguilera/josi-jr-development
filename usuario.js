9445429b16ec71c2c725b71027a62729124daa84
Josi Aguilera <josiaguilera@MacBook-Pro-de-Josi.local>
2024-03-30 12:38:31 -0300

    clase 2 idiomas

---
+++ usuario.js
@@ -0,0 +1,10 @@
+/* 
+-le pedimos al usuario que ingrese su nombre
+-almacenamos ese dato en memoria
+*/
+
+let nombre = prompt('ingrese su nombre');
+
+//imprimimos en el documento
+//document.write ('Bienvenido ', nombre )
+frase.innerText= nombre;
---
+++ variables.js
@@ -0,0 +1,28 @@
+/* 
+para declarar variables 
+utilizamos la palabra 
+reservada "let"
+*/
+
+let numero = 10;
+let frase = 'hola mundo' 
+
+//para declarar una constante utilizamos la palabra reservada const
+
+const nombre= JOSI
+
+// imprimimos valor de nuestras variables en el documento
+document.write (numero)
+document.write ("<br>")
+document.write (frase)
+document.write ("<br>")
+
+//sobrescribimos el valor de la variable numero
+numero=20;
+document.write(numero)
+frase= "javascript desde 0";
+document.write ("<br>")
+document.write (frase)
+document.write ("<br>")
+
+
